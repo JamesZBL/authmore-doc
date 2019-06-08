e3a50010eb67b35c20e067ce55a206db129dcf1d
摆码王子 <1146556298@qq.com>
2019-06-08 13:15:56 +0800

    change registry to docker hub

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-img_name='registry.cn-beijing.aliyuncs.com/letec/authmore-doc'
+img_name='jameszbl/authmore-doc'
 npm run docs:build
 cat ./deploy/analyse.txt >> ./docs/index.html
 docker build -f Dockerfile -t $img_name .
