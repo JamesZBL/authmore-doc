31cbfffb1078f62999e068b838c4cc5a4539b98e
摆码王子 <1146556298@qq.com>
2019-06-12 12:49:12 +0800

    update export script

--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p archive
-git archive --prefix=authmore-doc/ --format=zip --output archive/authmore-doc.zip \
+git archive --format=zip --output archive/authmore-doc.zip \
  --remote=git@gitee.com:zbl1996/authmore-doc.git master
